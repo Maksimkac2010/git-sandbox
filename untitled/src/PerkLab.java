39268b8c08bcc50e1f016e491394e4f8488dacf1
Maxim <udod.maxim2010@gmail.com>
2026-09-11 22:49:36 +0300

    fix(perklab): пофікси perklab

--- untitled/src/PerkLab.java
+++ untitled/src/PerkLab.java
@@ -1,3 +1,30 @@
+void isHere(String perkName, String[] perks) {
+    for (String perk : perks) {
+        if (perkName.equals(perk)) {
+            System.out.println("Here is a here.");
+            return;
+        }
+    }
+    System.out.println("Here is not a here.");
+
+}
+
+void longerForN(int n, String[] perks) {
+    int count = 0;
+    for (String perk : perks) {
+        if (perk.length() > n) {
+            count++;
+        }
+    }
+    System.out.println("Слів більше за " + n + " символів: " + count);
+}
+
 void main() {
     System.out.println("Hello World!");
+    String[] perks = {"two", "koil", "bowl", "zadar", "roma"};
+    for (String perk : perks) {
+        System.out.println(perk);
+    }
+    isHere("roma", perks);
+    longerForN(3, perks);
 }
